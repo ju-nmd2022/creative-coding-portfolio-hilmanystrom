007868388c3bfb462d6482c6deaedf45d9488a31
hilmanystrom <nyhi22bc@student.ju.se>
2024-08-30 15:31:20 +0200

    Second file

--- experiments/Hobbs1.js
+++ experiments/Hobbs1.js
@@ -34,11 +34,4 @@ function draw() {
       translate(x, y);
       flower();
 
-      pop();
-      x += flowerSize + gap;
-    }
-    y += flowerSize + gap;
-  }
-}
-
-// I used the code example we got in the workshop with Bassima on the 28/8 to help me get started //
+      po
