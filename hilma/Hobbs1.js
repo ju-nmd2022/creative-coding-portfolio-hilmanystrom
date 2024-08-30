b27db6a5c2978b0247a7346eb30d29c83182fa65
hilmanystrom <nyhi22bc@student.ju.se>
2024-08-30 15:25:07 +0200

    To commit

--- hilma/Hobbs1.js
+++ hilma/Hobbs1.js
@@ -11,18 +11,14 @@ function setup() {
 function flower() {
   noStroke();
   let petals = floor(random(5, 11));
-  let petalColor = color(random(200, 250), random(80, 100), random(100, 160));
 
   for (let y = 0; y < petals; y++) {
     for (let x = 0; x < petals; x++) {
-      fill(petalColor);
-      rect(0, 3, 8, 35);
+      fill(254, 219, 100);
+      ellipse(0, 3, 10, 80);
 
       fill(135, 83, 63);
       ellipse(0, 0, 10, 9);
-
-      fill(0);
-      rect(x, 30, 4);
 
       rotate(TWO_PI / petals); //Used ChatGPT to help me figure out the "TWO_PI" to make the petals smaller and variate more in size //
     }
@@ -38,11 +34,4 @@ function draw() {
       translate(x, y);
       flower();
 
-      pop();
-      x += flowerSize + gap;
-    }
-    y += flowerSize + gap;
-  }
-}
-
-// I used the code example we got in the workshop with Bassima on the 28/8 to help me get started //
+      po
--- hilma/Hobbs2.js
+++ hilma/Hobbs2.js
@@ -11,14 +11,18 @@ function setup() {
 function flower() {
   noStroke();
   let petals = floor(random(5, 11));
+  let petalColor = color(random(200, 250), random(80, 100), random(100, 160));
 
   for (let y = 0; y < petals; y++) {
     for (let x = 0; x < petals; x++) {
-      fill(254, 219, 100);
-      ellipse(0, 3, 10, 80);
+      fill(petalColor);
+      rect(0, 3, 8, 35);
 
       fill(135, 83, 63);
       ellipse(0, 0, 10, 9);
+
+      fill(0);
+      rect(x, 30, 4);
 
       rotate(TWO_PI / petals); //Used ChatGPT to help me figure out the "TWO_PI" to make the petals smaller and variate more in size //
     }
@@ -31,14 +35,4 @@ function draw() {
     let x = (height - flowerSize + amount - gap * (amount - 1)) / 2;
     for (let j = 0; j < amount; j++) {
       push();
-      translate(x, y);
-      flower();
-
-      pop();
-      x += flowerSize + gap;
-    }
-    y += flowerSize + gap;
-  }
-}
-
-// I used the code example we got in the workshop with Bassima on the 28/8 to help me get started //
+      tr
