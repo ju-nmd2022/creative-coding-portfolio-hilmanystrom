bbb43c464a08416441eab08db72509e784358ae2
hilmanystrom <nyhi22bc@student.ju.se>
2024-09-20 11:38:38 +0200

    maybe done with first one

--- experiments/Week2.js
+++ experiments/Week2.js
@@ -1,42 +1,43 @@
 let particles = [];
-let noiseScale = 0.02;
-let mouseAttractionStrength = 0.05;
+let mouseAttractionStrength = 0.02;
+let gravity;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
+  gravity = createVector(0, 0.05);
   noLoop();
 }
 
 class Particle {
   constructor(x, y) {
     this.position = createVector(x, y);
-    this.lifespan = 200 + Math.random() * 100;
+    this.velocity = p5.Vector.random2D();
+    this.velocity.mult(random(1, 3));
+    this.lifespan = 100 + Math.random() * 50;
     this.maxLifespan = this.lifespan;
-    this.size = random(10, 20);
-    this.hue = random(255);
-    this.noiseOffsetX = random(1000);
-    this.noiseOffsetY = random(1000);
-    this.velocity = createVector(0, 0);
+    this.size = random(4, 6);
+    this.hue = random(360);
+    this.burstFactor = random(1.1, 1.3);
   }
 
   update() {
     this.lifespan--;
 
-    let noiseX = noise(this.noiseOffsetX) * TWO_PI;
-    let noiseY = noise(this.noiseOffsetY) * TWO_PI;
-    let noiseVelocity = createVector(cos(noiseX), sin(noiseY));
-    noiseVelocity.mult(0.5);
-    this.position.add(noiseVelocity);
+    this.velocity.add(gravity);
 
-    this.noiseOffsetX += noiseScale;
-    this.noiseOffsetY += noiseScale;
+    if (this.lifespan > this.maxLifespan * 0.8) {
+      this.velocity.mult(this.burstFactor);
+    }
 
     let mouse = createVector(mouseX, mouseY);
     let attractionForce = p5.Vector.sub(mouse, this.position);
     attractionForce.mult(mouseAttractionStrength);
-    this.position.add(attractionForce);
+    this.velocity.add(attractionForce);
 
-    this.size *= 1;
+    this.velocity.mult(0.98);
+    this.position.add(this.velocity);
+
+    this.size *= 0.97;
 
     this.hue += 1;
   }
@@ -46,12 +47,13 @@ class Particle {
     translate(this.position.x, this.position.y);
     noStroke();
 
+    colorMode(RGB, 255);
+    let alpha = map(this.lifespan, 0, this.maxLifespan, 0, 255);
     let r = map(sin(this.hue * 0.1), -1, 1, 50, 255);
     let g = map(cos(this.hue * 0.1), -1, 1, 50, 255);
     let b = map(sin(this.hue * 0.2), -1, 1, 50, 255);
 
     fill(r, g, b, alpha);
-
     ellipse(0, 0, this.size);
     pop();
   }
@@ -63,9 +65,7 @@ class Particle {
 
 function generateParticles(x, y) {
   for (let i = 0; i < 100; i++) {
-    const px = x + random(-10, 10);
-    const py = y + random(-10, 10);
-    const particle = new Particle(px, py);
+    const particle = new Particle(x, y);
     particles.push(particle);
   }
 }
@@ -87,7 +87,7 @@ function drawParticles() {
 }
 
 function draw() {
-  background(0, 0, 0, 35);
+  background(0, 0, 0, 50);
   drawParticles();
 }
 
